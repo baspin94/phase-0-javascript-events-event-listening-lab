515ee93b70e7c67262919e1793354c7963b1d47a
Bianca Aspin <bianca@mj94.net>
2023-01-11 10:24:45 -0800

    Completed lab (Phase 1 review)

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const clickAlert = () => alert("I was clicked!");
+    const input = document.getElementById("button");
+    input.addEventListener('click', clickAlert);
 }
+
+addingEventListener();
